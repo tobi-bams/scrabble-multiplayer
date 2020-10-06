fde395fd292621bc131159068419acd9a36d685c
tobi-bams <teebams49@gmail.com>
2020-10-06 12:43:48 +0100

    the drag and drop function

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 let boxes = document.querySelectorAll(".box");
+let letters = document.querySelectorAll(".letters");
 let i = 1;
+let id = document.getElementById("1");
+let targetContainer = document.getElementById('target');
+let idLetter = 1;
+
+// targetContainer.addEventListener('dragover', (ev) => {
+//     ev.preventDefault();
+//     let id  = ev.dataTransfer.getData("text");
+//     console.log("happy");
+// })
 
 boxes.forEach(box => {
-    console.log(i);
-    console.log(box.textContent);
-    if(i === 106 || i === 120 || i === 225 || i === 211 || i === 218){
-        
-    }
-    box.setAttribute("draggable", "true");
     i+= 1;
+    box.addEventListener('dragover', (ev) => {
+        ev.preventDefault();
+        let id  = ev.dataTransfer.getData("text");
+        console.log("happy");
+    });
+    box.addEventListener('drop', (evt) => {
+        evt.preventDefault();
+        let id  = evt.dataTransfer.getData("text");
+        evt.target.appendChild(document.getElementById(id));
+    })
+})
+
+// id.addEventListener('dragstart', (e) => {
+//     e.dataTransfer.setData("Text", e.target.id);
+// })
+
+letters.forEach((letter) => {
+    letter.setAttribute("id", idLetter);
+    idLetter += 1;
+    letter.setAttribute("draggable", "true");
+    letter.addEventListener('dragstart', (evt) => {
+        evt.dataTransfer.setData("Text", evt.target.id);
+    })
+    
 })
